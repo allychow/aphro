753aa5ad8a0fd8534cdd776b5e44472439a3641b
Allison Chow <alchow@uwaterloo.ca>
2017-06-03 16:03:46 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.use(express.static(__dirname + '/views'));
+
+app.set('view engine', 'html'); // set up ejs for templating
+//app.use('/assets', express.static('assets'));
+
+// routes ======================================================================
+require('./app/routes.js'); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
